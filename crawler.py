ba8decf7084e3824860c5712b0a0cd26ccb54ec7
guetmatt <mguetebier@uni-osnabrueck.de>
2024-12-18 18:36:00 +0100

    index with whoosh

--- crawler.py
+++ crawler.py
@@ -3,27 +3,38 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 from collections import defaultdict
 
+from whoosh.index import create_in
+from whoosh.fields import *
+from whoosh import index
 
+
+
+
+
+
+### INDEX WITH WHOOSH ###
+schema = Schema(title=TEXT(stored=True), content=TEXT)
+index = create_in("indexdir", schema)
+writer = index.writer()
 
 
 
 
 ### INDEX SETUP ###
-index = defaultdict(set)
+# index = defaultdict(set)
 
-def add_content_to_index(content: str, url: str):
-    tokens = content.split()
-    for tok in tokens:
-        tok = tok.strip(".!?()[]{};:,\\/").lower()
-        index[tok].add(url)
+# def add_content_to_index(content: str, url: str):
+#     tokens = content.split()
+#     for tok in tokens:
+#         tok = tok.strip(".!?()[]{};:,\\/").lower()
+#         index[tok].add(url)
 
 
 ### SEARCH FUNCTION ###
 
 def search(index: dict, query: list):
-
     pages = set(index[query[0].lower()])
-    # print(pages)
+
     for word in query:
         pages = pages & set(index[word.lower()])
     
@@ -50,7 +61,8 @@ while agenda:
             
             # text content from url for index
             text = soup.get_text(" ", strip=True)
-            add_content_to_index(text, url)
+            # add_content_to_index(text, url)
+            writer.add_document(title=url, content=text)
 
             for page in soup.find_all("a"):
                 page_url = urljoin(url, page["href"])
@@ -64,36 +76,10 @@ while agenda:
     except requests.exceptions.RequestException as e:
         print(f"Error: not able to crawl url {url}")
 
-
-test = search(index, ["might", "home"])
-print(test)
-
+# write index to disk
+writer.commit()
 
 
+# test = search(index, ["might", "home"])
+# print(test)
 
-# ### INDEX WITH WOOSH ###
-
-# schema = Schema(title=TEXT(stored=True), content=TEXT)
-
-# # Create an index in the directory indexdr (the directory must already exist!)
-# index = create_in("indexdir", schema)
-# writer = index.writer()
-
-# # now let's add some texts (=documents)
-# writer.add_document(title=u"First document", content=u"This is the first document we've added!")
-# writer.add_document(title=u"Second document", content=u"The second one is even more interesting!")
-# writer.add_document(title=u"Songtext", content=u"Music was my first love and it will be the last")
-# writer.add_document(title=u"Test", content=u"This is the first and the last time I am gonna do this!")
-
-# # write the index to the disk
-# writer.commit()
-
-# # Retrieving data
-# with index.searcher() as searcher:
-#     # find entries with the words 'first' AND 'last'
-#     query = QueryParser("content", index.schema).parse("first last")
-#     results = searcher.search(query)
-
-#     # print all results
-#     for r in results:
-#         print(r)
